4f85a323f9f3eb5ef9b2be3638482c167bcebee3
Teigen Leonard <teigenl@gmail.com>
2017-08-15 00:01:47 -0500

    added html and vote function

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -30,7 +30,7 @@ export class HomePage {
         return Math.min(Math.abs(offsetX) / (element.offsetWidth/2), 1);
       },
       transform: (element, x, y, r) => {
-        // this.onItemMove(element, x, y, r);
+        this.onItemMove(element, x, y, r);
       },
       throwOutDistance: (d) => {
         return 800;
@@ -38,4 +38,62 @@ export class HomePage {
     };
   }
 
+  ngAfterViewInit() {
+    // Either subscribe in controller or set in HTML
+    // this.swingStack.throwin.subscribe((event: DragEvent) => {
+    //   event.target.style.background = '#ffffff';
+    // });
+
+    this.cards = [{email: ''}];
+    this.addNewCards(1);
+  }
+
+  onItemMove(element, x, y, r) {
+    var color = '';
+    var abs = Math.abs(x);
+    let min = Math.trunc(Math.min(16*16 - abs, 16*16));
+    let hexCode = this.decimalToHex(min, 2);
+
+    if (x < 0) {
+       color = '#FF' + hexCode + hexCode;
+     } else {
+       color = '#' + hexCode + 'FF' + hexCode;
+     }
+
+     element.style.background = color;
+     element.style['transform'] = `translate3d(0, 0, 0) translate(${x}px, ${y}px) rotate(${r}deg)`;
+  }
+
+  decimalToHex(d, padding) {
+    var hex = Number(d).toString(16);
+    padding = typeof (padding) === "undefined" || padding === null ? padding = 2 : padding;
+
+    while (hex.length < padding) {
+      hex = "0" + hex;
+    }
+
+    return hex;
+  }
+
+  // Add new cards to our array
+  addNewCards(count: number) {
+    this.http.get('https://randomuser.me/api/?results=' + count)
+    .map(data => data.json().results)
+    .subscribe(result => {
+      for (let val of result) {
+        this.cards.push(val);
+      }
+    })
+  }
+
+  // Connected through HTML
+  voteUp(like: boolean) {
+    let removedCard = this.cards.pop();
+    this.addNewCards(1);
+    if (like) {
+      this.recentCard = 'You liked: ' + removedCard.email;
+    } else {
+      this.recentCard = 'You disliked: ' + removedCard.email;
+    }
+  }
 }
