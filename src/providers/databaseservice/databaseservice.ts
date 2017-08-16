d5257977e407c24bb499b47714b69e152ef99ba8
Teigen Leonard <teigenl@gmail.com>
2017-08-16 11:12:16 -0500

    debugged firebase auth

--- src/providers/databaseservice/databaseservice.ts
+++ src/providers/databaseservice/databaseservice.ts
@@ -2,6 +2,16 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
+import { AngularFireModule } from 'angularfire2';
+// For Database
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+import { AngularFireDatabase } from 'angularfire2/database';
+// For Auth
+import {AngularFireAuthModule} from 'angularfire2/auth';
+import { AngularFireAuth } from 'angularfire2/auth';
+// For Observables
+import { FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
+
 
 /*
   Generated class for the DatabaseserviceProvider provider.
@@ -16,4 +26,13 @@ export class DatabaseserviceProvider {
     console.log('Hello DatabaseserviceProvider Provider');
   }
 
+}
+
+export class Databaseservice {
+   constructor(private_af: AngularFireModule) {
+   }
+   public listAccounts(): FirebaseListObservable<any[]>{
+      console.log(this)
+    return this
+   }
 }
